3c16e5f02aadbcb394fd86a83d80eab7ea173730
teresa0919 <a0936170460@gmail.com>
2025-04-15 02:59:33 +0800

    Add files via upload

--- server.js
+++ server.js
@@ -3,17 +3,17 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 
-// 提供靜態檔案 (CSS、JS、HTML)
+// 提供靜態資源 (CSS、JS、HTML)
 app.use(express.static(__dirname));
 
-// 根目錄打開 index.html
+// 首頁
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// 啟動 server，並且監聽 PORT
+// 啟動伺服器
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on port ${PORT}`);
 });
