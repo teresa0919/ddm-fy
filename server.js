5eefc0c72b3631dc16e9e4d12d85c0b2f2307fdf
teresa0919 <a0936170460@gmail.com>
2025-04-14 23:37:19 +0800

    Add files via upload

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
-// Node.js Express 伺服器
+
+const express = require('express');
+const app = express();
+const path = require('path');
+
+// 把當前資料夾設為靜態檔案目錄
+app.use(express.static(__dirname));
+
+// 首頁路由：送出 index.html
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// 讓 Render 或本地環境可以自動選 Port
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
