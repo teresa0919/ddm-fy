3edfcfde53388727a7b36e70e837b9f55596e207
teresa0919 <a0936170460@gmail.com>
2025-04-15 02:21:52 +0800

    Delete server (3).js

--- server.js
+++ server.js
@@ -1,18 +1,1 @@
 
-const express = require('express');
-const path = require('path');
-const app = express();
-
-// 讓 express 直接服務靜態檔案
-app.use(express.static(__dirname));
-
-// 首頁
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
-});
-
-// 啟動伺服器，監聽 PORT
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`伺服器啟動囉，port: ${port}`);
-});
